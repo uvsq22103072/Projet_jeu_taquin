b88ef64df45eedd88d1a8a86b0f47ffbbd2dd9a3
Lucas BEDEL <lucas.bedel@ens.uvsq.fr>
2022-04-14 11:27:42 +0200

    ok

--- jeutaquin.py
+++ jeutaquin.py
@@ -6,10 +6,13 @@ from random import randint #choisir un nombre aléatoirement
 
 LARGEUR = 400 #Car chaque ligne a 4 cases de 100px chacune
 HAUTEUR = 400 #Car il y a 4 lignes et chaque ligne fait 100px
-
+mouvement = 0 #Défini une fois fini le nombre de mouvement effectué pour gagné.
 ###################
 # Fonctions
-
+def identification(event):
+    i=event.y//100
+    j=event.x//100
+    print("ligne :", i, "colonne :", j,"Tuile n° :", taquin[i][j])
 
 
 ######################
@@ -22,11 +25,35 @@ racine.title("Jeu du Taquin")
 
 
 # création des tuiles
+FONT=('Arial', 30, 'bold')
+
+taquin=[[1, 2, 3, 4],
+       [5, 6, 7, 8],
+       [9, 10, 11, 12],
+       [13, 14, 15, 16]]
+
+canvas.bind("<Button-1>",identification)
+
+tuile=[None for i in range(17)]
+
+for i in range(4):
+    for j in range(4):
+        x, y=100*j, 100*i
+        A, B, C=(x, y), (x+100, y+100), (x+50, y+50)
+        rect=canvas.create_rectangle(A, B, fill="red")
+        numero=taquin[i][j]
+        txt=canvas.create_text(C, text=numero, fill="black", font=FONT)
+        tuile[numero]=(rect, txt)
+canvas.delete(rect)
+canvas.delete(txt)
 
 
+taquin_victoire=[[1, 2, 3, 4],
+                [5, 6, 7, 8],
+                [9, 10, 11, 12],
+                [13, 14, 15, 16]]
 
-
-
+canvas.pack()
 # déplacement des tuiles
 
 
